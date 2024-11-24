4d98ca071cffa2b59ebc4dd86e095758c2ff0c19
Hassan Jamshaid <hassanjamshaid364@gmail.com>
2024-11-24 14:33:42 +0500

    5 done

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
-import RoomJoinPage from "./RoomJoinPage.js";
-import CreateRoomPage from "./CreateRoomPage.js";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Router>
-                <Routes>
-                    <Route path="/" element={<p>This is the home page</p>} />
-                    <Route path="/join" element={<RoomJoinPage />} />
-                    <Route path="/create" element={<CreateRoomPage />} />
-                </Routes>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
